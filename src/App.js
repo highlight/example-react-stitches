8763b5674d0a8ebbd2c0e8023fb4c351699ab964
John Pham <john@highlight.run>
2021-07-30 14:28:09 -0700

    Add some styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,77 @@
+import { useState } from "react";
+import { styled } from "./stitches.config";
+
 function App() {
+  const [showGif, setShowGif] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main>
+      <h1>Highlight Demo</h1>
+      <h2>
+        A create-react-app using <code>@stitches/react</code>
+      </h2>
+      <Container>
+        <Button>A fancy button</Button>
+        <Button className="custom-class">A fancy button</Button>
+        <ButtonWithPseudoElement>Pseudo Element</ButtonWithPseudoElement>
+      </Container>
+      <Container>
+        <Button
+          onClick={() => {
+            setShowGif((previousValue) => !previousValue);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Suprise me!
+        </Button>
+      </Container>
+      {showGif && (
+        <div>
+          <img
+            src="https://media.giphy.com/media/BzyTuYCmvSORqs1ABM/giphy.gif"
+            alt=""
+          />
+        </div>
+      )}
+    </main>
   );
 }
 
 export default App;
+
+const Container = styled("div", {
+  display: "flex",
+  gap: "12px",
+  marginBottom: "36px",
+});
+
+const Button = styled("button", {
+  backgroundColor: "$primaryBrand",
+  color: "white",
+  borderRadius: "9999px",
+  border: 0,
+  fontSize: "13px",
+  padding: "10px 15px",
+  "&:hover": {
+    backgroundColor: "lightgray",
+  },
+  "&.custom-class": {
+    boxShadow: "0 0 0 3px blueviolet",
+  },
+});
+
+const ButtonWithPseudoElement = styled(Button, {
+  position: "relative",
+
+  "&::before": {
+    content: `''`,
+    display: "block",
+    backgroundImage: "linear-gradient(to right, #1fa2ff, #12d8fa, #a6ffcb)",
+    position: "absolute",
+    top: "-3px",
+    left: "-3px",
+    width: "calc(100% + 6px)",
+    height: "calc(100% + 6px)",
+    borderRadius: "inherit",
+    zIndex: -1,
+  },
+});
---
+++ src/stitches.config.js
@@ -0,0 +1,21 @@
+import { createCss } from "@stitches/react";
+
+export const { styled, css, global, keyframes, getCssString, theme } =
+  createCss({
+    theme: {
+      colors: {
+        gray400: "gainsboro",
+        gray500: "lightgray",
+        primaryBrand: "#5629c6",
+      },
+    },
+    media: {
+      bp1: "(min-width: 480px)",
+    },
+    utils: {
+      marginX: (config) => (value) => ({
+        marginLeft: value,
+        marginRight: value,
+      }),
+    },
+  });
